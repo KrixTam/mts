c772e946f4296d228f1f431e0b6e80bd18598e67
Krix Tam <krix.tam@qq.com>
2022-02-05 00:07:48 +0800

    增加coverage批跑测试的脚本

---
+++ run_coverage.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+import coverage
+import unittest
+
+
+def run_all_tests(test_modules):
+    suite = unittest.TestSuite()
+    for t in test_modules:
+        try:
+            # If the module defines a suite() function, call it to get the suite.
+            mod = __import__(t, globals(), locals(), ['suite'])
+            suite_fn = getattr(mod, 'suite')
+            suite.addTest(suite_fn())
+        except (ImportError, AttributeError):
+            # else, just load all the test cases from the module.
+            suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+    unittest.TextTestRunner().run(suite)
+
+
+# 删除测试输出目录
+dirs = ['log', 'output', 'htmlcov']
+for d in dirs:
+    if os.path.exists(d):
+        shutil.rmtree(d)
+
+test_modules = [
+    'mts.test.test_DBHandler',
+    'mts.test.test_DataDictionary',
+    'mts.test.test_DataDictionaryId',
+    'mts.test.test_DataUnitService',
+    'mts.test.test_ObjectId',
+    'mts.test.test_SpaceDataUnit',
+    'mts.test.test_TimeDataUnit'
+]
+
+# 执行测试用例
+cov = coverage.Coverage()
+cov.start()
+
+run_all_tests(test_modules)
+
+cov.stop()
+cov.save()
+
+cov.html_report(directory='htmlcov')
